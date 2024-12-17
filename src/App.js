be156c66340086c372f4dbc986ff5b85ced64fe8
Your Name <akshitamoradiya2929@gmail.com>
2024-12-17 14:48:32 +0530

    update

--- src/App.js
+++ src/App.js
@@ -57,14 +57,14 @@ export default function App() {
   const { pathname } = useLocation();
 
   // Cache for the rtl
-  useMemo(() => {
-    const cacheRtl = createCache({
-      key: "rtl",
-      stylisPlugins: [rtlPlugin],
-    });
+  // useMemo(() => {
+  //   const cacheRtl = createCache({
+  //     key: "rtl",
+  //     stylisPlugins: [rtlPlugin],
+  //   });
 
-    setRtlCache(cacheRtl);
-  }, []);
+  //   setRtlCache(cacheRtl);
+  // }, []);
 
   // Open sidenav when mouse enter on mini sidenav
   const handleOnMouseEnter = () => {
